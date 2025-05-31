7d56504e32e2bb44f2ec071c4e485a37d5cef07d
Ruslan Yarema <ryarema@cisco.com>
2025-05-31 21:39:48 +0300

    Added remote backend

--- terraform/jfrog-cr/main.tf
+++ terraform/jfrog-cr/main.tf
@@ -1,16 +1,16 @@
-# terraform {
-#   backend "gcs" {
-#     bucket = ""
-#     prefix = ""
-#   }
-#   required_version = ">= 1.12.1"
-#   required_providers {
-#     google = {
-#       source  = "hashicorp/google"
-#       version = "~> 5.30.0"
-#     }
-#   }
-# }
+terraform {
+  backend "gcs" {
+    bucket = "us-central1-jcr-tf-state"
+    prefix = "terraform/init"
+  }
+  required_version = ">= 1.12.1"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.37.0"
+    }
+  }
+}
 
 provider "google" {
   project = var.project
