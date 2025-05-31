fa2b8d2201eba9a9690565bdee6fc0c510b1ae80
Ruslan Yarema <ryarema@cisco.com>
2025-05-31 14:21:36 +0300

    Temporary commented tfstate bcuket, updated tf version

--- terraform/jfrog-cr/main.tf
+++ terraform/jfrog-cr/main.tf
@@ -1,16 +1,16 @@
-terraform {
-  backend "gcs" {
-    bucket = ""
-    prefix = ""
-  }
-  required_version = ">= 1.8.4"
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.30.0"
-    }
-  }
-}
+# terraform {
+#   backend "gcs" {
+#     bucket = ""
+#     prefix = ""
+#   }
+#   required_version = ">= 1.12.1"
+#   required_providers {
+#     google = {
+#       source  = "hashicorp/google"
+#       version = "~> 5.30.0"
+#     }
+#   }
+# }
 
 provider "google" {
   project = var.project
